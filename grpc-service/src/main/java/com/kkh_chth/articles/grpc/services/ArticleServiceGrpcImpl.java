1989b008543dc1a1ff31cc943f022c12d5dfebde
rahul.gorad <rahul.gorad>
2024-05-11 16:46:21 +0530

    build: mock response add

--- grpc-service/src/main/java/com/kkh_chth/articles/grpc/services/ArticleServiceGrpcImpl.java
+++ grpc-service/src/main/java/com/kkh_chth/articles/grpc/services/ArticleServiceGrpcImpl.java
@@ -1,7 +1,6 @@
 package com.kkh_chth.articles.grpc.services;
 
 import com.kkh_chth.articles.grpc.proto.*;
-import io.grpc.CallOptions;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 import reactor.core.publisher.Mono;
@@ -12,33 +11,45 @@ public class ArticleServiceGrpcImpl extends ReactorArticleServiceGrpc.ArticleSer
 
     @Override
     public Mono<CreateArticleResponse> createArticle(Mono<CreateArticleRequest> request) {
-        return Mono.just(CreateArticleResponse.newBuilder().setArticle(Article.newBuilder().setId(12L).setAuthor("dummy author").setCategory("grpc gradle").build()).build());
+        // write your own logic here
+        log.info("Inside createArticle service");
+        return Mono.just(CreateArticleResponse.newBuilder().setArticle(Article.newBuilder().setId(3L).setTitle("Streamlining Development: Constructing a gRPC Gradle Spring WebFlux Project").setAuthor("KKH CHTH").setCategory("GRPC-GRADLE-REACTIVE").build()).build());
     }
 
     @Override
     public Mono<GetArticleByIdResponse> getArticleById(Mono<GetArticleByIdRequest> request) {
+        // write your own logic here
+        log.info("Inside getArticleById service");
         return Mono.just(GetArticleByIdResponse.newBuilder()
-                .setArticle(Article.newBuilder().setId(12L).setAuthor("dummy author").setCategory("getArticleById")
-                        .build()).build());
+                .setArticle(Article.newBuilder().setId(3L).setTitle("Streamlining Development: Constructing a gRPC Gradle Spring WebFlux Project").setAuthor("KKH CHTH").setCategory("GRPC-GRADLE-REACTIVE").build()).build());
 
     }
 
     @Override
+
     public Mono<GetAllArticlesResponse> getAllArticles(Mono<Empty> request) {
+        // write your own logic here
+        log.info("Inside getAllArticles service");
         return  Mono.just(GetAllArticlesResponse.newBuilder()
-                .addArticles(Article.newBuilder().setId(12L).setAuthor("dummy author").setCategory("getAllArticles"))
+                .addArticles(Article.newBuilder().setId(1L).setTitle("Seamless Solutions: Crafting a gRPC Gradle Spring Boot Project").setAuthor("KKH CHTH").setCategory("GRPC-GRADLE").build())
+                .addArticles(Article.newBuilder().setId(2L).setTitle("Efficient Engineering: Building a gRPC Maven Spring Boot Project").setAuthor("KKH CHTH").setCategory("GRPC-MAVEN").build())
+                .addArticles(Article.newBuilder().setId(3L).setTitle("Streamlining Development: Constructing a gRPC Gradle Spring WebFlux Project").setAuthor("KKH CHTH").setCategory("GRPC-GRADLE-REACTIVE").build())
+                .addArticles(Article.newBuilder().setId(4L).setTitle("Optimal Integration: Developing a gRPC Maven Spring WebFlux Project").setAuthor("KKH CHTH").setCategory("GRPC-MAVEN-REACTIVE").build())
                         .build());
     }
 
     @Override
     public Mono<UpdateArticleResponse> updateArticle(Mono<UpdateArticleRequest> request) {
+        // write your own logic here
+        log.info("Inside updateArticle service");
         return Mono.just(UpdateArticleResponse.newBuilder()
-                .setArticle(Article.newBuilder().setId(12L).setAuthor("dummy author").setCategory("updateArticle")
-                        .build()).build());
+                .setArticle(Article.newBuilder().setId(3L).setTitle("Streamlining Development: Constructing a gRPC Gradle Spring WebFlux Project").setAuthor("KKH CHTH").setCategory("GRPC-GRADLE-REACTIVE").build()).build());
     }
 
     @Override
     public Mono<DeleteArticleResponse> deleteArticle(Mono<DeleteArticleRequest> request) {
+        // write your own logic here
+        log.info("Inside deleteArticle service");
         return Mono.just(DeleteArticleResponse.newBuilder().build());
     }
 
